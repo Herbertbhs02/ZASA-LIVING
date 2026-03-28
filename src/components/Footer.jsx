cb15cfd63a9b833ed9dae511b268935ae1bc5d1a
Herbert Ssevume <you@example.com>
2026-03-28 10:31:58 +0000

    footer and index css change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="bg-gray-600 text-white p-4 mt-10">
     
            
-              <div className="flex justify-center gap-6">
+              <div className="flex justify-center py-1 px-1 gap-2">
                   <a href="mailto:h.sev@tiscali.co.uk" className='block'>zasa-living@gmail.com </a>
                   <a href="/my-facebook-link"><FontAwesomeIcon icon={faFacebook} size="2x"className="text-blue-500"/> </a>
                   <a href="/my-twitter-link"><FontAwesomeIcon icon={faTwitter} size="2x" className="text-sky-400" /></a>
