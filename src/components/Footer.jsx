8aefac70d6a77d06d573e9d6ddbbc8d5232b7f34
Herbert Ssevume <you@example.com>
2026-03-29 19:36:42 +0100

    Added facebook link in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,10 @@ const Footer = () => {
            
               <div className="flex justify-center py-1 px-1 gap-2">
                   <p className='block text-sm'>zasaservices25@gmail.com </p>
-                  <a href="/my-facebook-link"><FontAwesomeIcon icon={faFacebook} size="2x"className="text-blue-500"/> </a>
+                  <a href="https://www.facebook.com/profile.php?id=61580788449763"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faFacebook} size="2x"className="text-blue-500"/> </a>
                   <a href="/my-twitter-link"><FontAwesomeIcon icon={faTwitter} size="2x" className="text-sky-400" /></a>
                   <a href="/my-youtube-link"><FontAwesomeIcon icon={faYoutube} size="2x" className="text-red-500"  /></a>
                   <a href="/my-youtube-link"><FontAwesomeIcon icon={faLinkedin} size="2x" className="text-[#0A66C2]" /></a>
