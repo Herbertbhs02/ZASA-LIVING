6da46ec8f12f0da5c9d8da839492550dd6f411ca
Herbert Ssevume <you@example.com>
2026-05-03 14:18:11 +0100

    about page removed content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,17 +3,7 @@ import React from "react";
 const About = () => {
   return (
      <div className="p-4">
-      <img
-        src="images/lifestyle.png"
-        alt=""
-        className="float-left mr-4 mb-2 w-40"
-      />
-
-      <p>
-        This text should start to the right of the image. As it continues,
-        it will wrap underneath the image once it reaches the bottom.
-        Keep adding more text here to see the effect clearly.
-      </p>
+     <h1>Page is currently undergoing update</h1>
     </div>
   );
 };
